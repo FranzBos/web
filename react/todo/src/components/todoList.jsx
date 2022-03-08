02d1bb9a32c25bc89e911d83881b25afb42d9992
Francesco <Francesco>
2022-03-08 15:03:47 +0100

    renders state list items

--- react/todo/src/components/todoList.jsx
+++ react/todo/src/components/todoList.jsx
@@ -2,13 +2,22 @@ import { render } from "@testing-library/react";
 import React, { Component } from "react";
 
 class TodoList extends Component {
+    state = {
+        items:[
+            "teach dog to do poop outside",
+            "clean bike",
+            "buy eggs",
+            "argue with girlfriend"
+        ]
+    }
+
     render() { return (
         <React.Fragment>
             <h1>Todo things</h1>
             <ul>
-                <li>Teach dog to do poop outside</li>
-                <li>Clean desk</li>
-                <li>Buy vegetables</li>
+                <li>{this.state.items[0]}</li>
+                <li>{this.state.items[1]}</li>
+                <li>{this.state.items[2]}</li>
             </ul>
         </React.Fragment> 
     )}
