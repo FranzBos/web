d6e3f93ff0c3e58d04e52be1a4c9cd58a29b4d18
Francesco <Francesco>
2022-03-08 14:58:11 +0100

    replace main div with react fragment

--- react/todo/src/components/todoList.jsx
+++ react/todo/src/components/todoList.jsx
@@ -3,14 +3,14 @@ import React, { Component } from "react";
 
 class TodoList extends Component {
     render() { return (
-        <div>
+        <React.Fragment>
             <h1>Todo things</h1>
             <ul>
                 <li>Teach dog to do poop outside</li>
                 <li>Clean desk</li>
                 <li>Buy vegetables</li>
             </ul>
-        </div> 
+        </React.Fragment> 
     )}
 }
 
